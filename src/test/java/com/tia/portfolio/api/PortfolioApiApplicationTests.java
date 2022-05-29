35afb23a6ea45501ceb64cfb2682298b4579fd5d
Yanggum <bak405@naver.com>
2022-05-29 18:03:00 +0900

    [2022-05-29 Yanggum] build.gradle 변경

--- src/test/java/com/tia/portfolio/api/PortfolioApiApplicationTests.java
+++ src/test/java/com/tia/portfolio/api/PortfolioApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PortfolioApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
